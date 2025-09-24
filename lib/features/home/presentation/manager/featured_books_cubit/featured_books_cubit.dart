7ad0bcbd6a139a7f10b5c87ca2a029f57b0d4987
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2025-09-25 00:03:38 +0300

    create featured books cubit

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState>{
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+
+}
